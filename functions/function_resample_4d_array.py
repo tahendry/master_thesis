378d092840e44b64f2761975876c9a03499752b4
Reto (Linux-WS) <reto.hendry@hslu.ch>
2023-04-20 13:00:42 +0200

    rename, make more efficient and readable

--- functions/function_resample_4d_array.py
+++ functions/function_resample_4d_array.py
@@ -4,11 +4,14 @@ Date: 2023-03-25
 """
 
 import numpy as np
+import tqdm
 
-def reshape_array(big_array_4d, reshape_cube):
+def resample_4d_array(big_array_4d, reshape_cube):
     """
-    Reshapes the bigger 3D array into a smaller 3D array by taking the mean of a volume
-    from the bigger array and using the mean as one element of the smaller array.
+    This code defines a function that resamples a 3D NumPy array 
+    by taking the mean of non-overlapping volumes of 
+    a specified size. The input array is first padded to ensure that 
+    it can be evenly divided into the smaller volumes.
     
     Parameters:
     ----------
@@ -25,7 +28,7 @@ def reshape_array(big_array_4d, reshape_cube):
 
     """
 
-    def reshape_3d_array(big_array, volume_size):
+    def resample_3d_array(big_array, volume_size):
         big_array_size = big_array.shape
         
         # Calculate the padding needed to make the big array a multiple of the volume size
@@ -37,29 +40,34 @@ def reshape_array(big_array_4d, reshape_cube):
                                             (padding[2], padding[2])])
         
         # Calculate the size of the small array
-        small_array_size = tuple(int(padded_big_array.shape[i] / volume_size[i]) for i in range(3))
-        small_array = np.zeros(small_array_size)
+        small_4d_array_size = tuple(int(padded_big_array.shape[i] / volume_size[i]) for i in range(3))
         
-        # Iterate over each element of the smaller array
-        for i in range(small_array_size[0]):
-            for j in range(small_array_size[1]):
-                for k in range(small_array_size[2]):
-                    # Calculate the mean of the volume from the bigger array
-                    volume_mean = np.mean(padded_big_array[i*volume_size[0]:(i+1)*volume_size[0],
-                                                        j*volume_size[1]:(j+1)*volume_size[1],
-                                                        k*volume_size[2]:(k+1)*volume_size[2]])
-                    # Assign the mean to the corresponding index in the smaller array
-                    small_array[i, j, k] = volume_mean
+        # Create a view of the bigger array with the desired volume size and the proper strides
+        strides = tuple(padded_big_array.strides[i] * volume_size[i] for i in range(3)) + padded_big_array.strides
+        window_view = np.lib.stride_tricks.as_strided(padded_big_array, shape=small_4d_array_size + volume_size, strides=strides)
         
-        return small_array
+        # Calculate the mean of each volume and reshape the result to the desired small array size
+        small_4d_array = np.mean(window_view, axis=(-3, -2, -1)).reshape(small_4d_array_size)
+        
+        return small_4d_array
     
     # Create a tuple with the volume size
     volume_size = (reshape_cube, reshape_cube, reshape_cube)
 
     # call the function
-    small_4d_array = np.array(
-        [reshape_3d_array(big_array_4d[i], volume_size) 
-            for i in range(big_array_4d.shape[0])]
-    )
+    # small_4d_array = np.array(
+    #     [reshape_3d_array(big_array_4d[i], volume_size) 
+    #         for i in range(big_array_4d.shape[0])]
+    # )
+
+    # call the function
+    print("Resampling in progress...")
+    small_4d_array = []
+    for i in tqdm.tqdm(range(big_array_4d.shape[0])):
+        reshaped_array = resample_3d_array(big_array_4d[i], volume_size)
+        small_4d_array.append(reshaped_array)
+
+    small_4d_array = np.array(small_4d_array)
     
+
     return small_4d_array
