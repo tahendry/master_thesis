bb26968988730ec72e669741dc8e85d786b3b94c
Reto (Linux-WS) <reto.hendry@hslu.ch>
2023-05-05 12:04:07 +0200

    voxel with value = 0 are invisible

--- functions/function_plot_3d_array_colored.py
+++ functions/function_plot_3d_array_colored.py
@@ -6,7 +6,6 @@ Author: Reto Hendry
 
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 def plot_3d_array_colored(array, marked_indices=None, linewidth_voxel=0):
     """
@@ -35,8 +34,15 @@ def plot_3d_array_colored(array, marked_indices=None, linewidth_voxel=0):
     cmap = plt.get_cmap('viridis')
     norm = plt.Normalize(array.min(), array.max())
     
-    filled = np.ones(array.shape, dtype=bool)
+    filled = (array != 0)  # Update this line to consider only non-zero values
     facecolors = cmap(norm(array))
+    
+    # Set alpha channel to 0 where array values are 0
+    facecolors[array == 0, -1] = 0.0
+
+    # Set the alpha value for non-transparent voxels
+    facecolors[array != 0, -1] = 0.6
+
     
     if marked_indices is not None:
         marked_facecolors = np.copy(facecolors)
@@ -47,14 +53,17 @@ def plot_3d_array_colored(array, marked_indices=None, linewidth_voxel=0):
         marked_facecolors[:,:,-1] = 0.8
         ax.voxels(marked_filled, facecolors=marked_facecolors, edgecolor='r', linewidth=1, alpha=None)
         
-    ax.voxels(filled, facecolors=facecolors, edgecolor='k', linewidth=linewidth_voxel, alpha=0.6)
+    ax.voxels(filled, facecolors=facecolors, edgecolor='k', linewidth=linewidth_voxel, alpha=None)
     
     ax.set_xlabel('X axis')
     ax.set_ylabel('Y axis')
     ax.set_zlabel('Z axis')
+
+    ax.view_init(elev=25, azim=-110)
     
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
     fig.colorbar(sm, ax=ax, shrink=0.8)
+    plt.tight_layout()
 
     plt.show()
